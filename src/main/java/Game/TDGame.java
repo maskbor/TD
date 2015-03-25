743cae058a3207a59602d04ebb22906cbd016395
redkachyov <maskbor@gmail.com>
2015-03-25 09:56:45 +0600

    добавлен генератор

--- src/main/java/Game/TDGame.java
+++ src/main/java/Game/TDGame.java
@@ -1,9 +1,13 @@
 package Game;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 public class TDGame implements ApplicationListener {
 
@@ -11,8 +15,12 @@ public class TDGame implements ApplicationListener {
 	private SpriteBatch batch;
     private TextureRegion wall;
     private TextureRegion road;
-    private int SIZE_MAP = 48;
-    private Cell[][] bmap;
+    private TextureRegion base;
+    private TextureRegion baseVrag;
+    private TextureRegion gun;
+
+    private int SIZE_MAP = 60;
+    private Cell[][] bmap = new Cell[SIZE_MAP][SIZE_MAP];
 	
     @Override
     public void create() {   
@@ -20,18 +28,24 @@ public class TDGame implements ApplicationListener {
     	batch = new SpriteBatch();
     	wall = new TextureRegion(fon, 0, 0, 10, 10);
         road = new TextureRegion(fon,10,0,10,10);
-
-
+        base = new TextureRegion(fon,20,0,10,10);
+        baseVrag = new TextureRegion(fon,30,0,10,10);
+        gun = new TextureRegion(fon,40,0,10,10);
         Generator g = new Generator(SIZE_MAP);
-        boolean[][] map = g.getMaze();
+        int[][] map = g.getMaze();
         bmap = new Cell[SIZE_MAP][SIZE_MAP];
 
         for (int i = 0; i < SIZE_MAP; i++)
             for (int j = 0; j < SIZE_MAP; j++) {
-                if (map[i][j] == false)
-                    bmap[i][j] = new Cell(road);
-                if (map[i][j] == true)
-                    bmap[i][j] = new Cell(wall);
+                if (map[i][j] == 0) {
+                    bmap[i][j] = new Road(road);
+                }
+                if (map[i][j] == 1)
+                    bmap[i][j] = new Wall(wall);
+                if(map[i][j] == 2)
+                    bmap[i][j] = new Base(base,false);
+                if(map[i][j] == 3)
+                    bmap[i][j] = new Base(baseVrag,true);
             }
     }
 
@@ -39,12 +53,14 @@ public class TDGame implements ApplicationListener {
     public void dispose() {
     }
 
+
     @Override
     public void render() {
+        update();
         batch.begin();
         for (int i = 0; i < SIZE_MAP; i++)
             for (int j = 0; j < SIZE_MAP; j++)
-                bmap[i][j].draw(batch, i*10, j*10);
+                bmap[i][j].draw(batch, i*10+5, j*10+5);
         batch.end();
     }
 
@@ -59,4 +75,13 @@ public class TDGame implements ApplicationListener {
     @Override
     public void resume() {
     }
+
+    public void update(){
+        boolean isTouched = Gdx.input.isTouched();
+        if(isTouched) {
+            //bmap[Gdx.input.getX()/10][59-Gdx.input.getY()/10]
+            bmap[Gdx.input.getX()/10][59-Gdx.input.getY()/10]=new Tower(gun);
+        }
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,16 @@
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import Game.MyTestGame;
+import Game.TDGame;
 
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "test-game";
+        cfg.title = "TD";
         cfg.useGL30=false;
         //cfg.useGL20 = false;
-        cfg.width = 480;
-        cfg.height = 320;
-        new LwjglApplication(new MyTestGame(), cfg);
+        cfg.width = 600;
+        cfg.height = 600;
+        new LwjglApplication(new TDGame(), cfg);
     }
 }
